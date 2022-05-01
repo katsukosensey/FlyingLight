7b8408449c85796f8c0f3009a09bf2e3f178ff09
dpimenova <dpimenova@sprintsoft.ru>
2022-05-01 14:30:47 +0300

    Добавила проект, механику, ассеты для сцены

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,179 @@
+using System;
+using UnityEngine;
+using Random = System.Random;
+
+public class BallController : MonoBehaviour
+{
+    private Random _random;
+    private float _currentAcceleration;
+    public int Speed = 1;
+    public int DefaultAcceleration = 1;
+    public int MinAcceleration = 1;
+    public float AccelerationDelta = 0.1f;
+    public int SecureMouseDistance = 2;
+    public Vector2 Direction = Vector2.right;
+    public GameObject Bottom;
+    public GameObject BottomCheck;
+    public GameObject Top;
+    public GameObject TopCheck;
+
+    public bool CanRun;
+    /// <summary>
+    /// В процессе перестроения движения
+    /// </summary>
+    public bool IsAvoiding;
+    public bool CatchedByMouse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _random = new Random();
+    }
+
+    public void ResetPosition(Vector3 position)
+    {
+        CanRun = false;
+        transform.position = position;
+        ResetTranslationParams();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsAvoiding)
+        {
+            return;
+        }
+
+        if (!CanRun)
+        {
+            return;
+        }
+
+        if (BottomCheck.transform.position.y < Bottom.transform.position.y)
+        {
+            Debug.LogWarning("Ball out of bottom");
+            transform.position = new Vector3(transform.position.x, Bottom.transform.position.y + 1);
+        }
+        if (TopCheck.transform.position.y > Top.transform.position.y)
+        {
+            Debug.LogWarning("Ball out of top");
+            transform.position = new Vector3(transform.position.x, Top.transform.position.y - 1);
+        }
+
+        if (CatchedByMouse)
+        {
+            if (IsMouseDistanceSecure())
+            {
+                Debug.Log($"Position is secure");
+                ResetTranslationParams();
+                Reset();
+            }
+            else
+            {
+                _currentAcceleration+= AccelerationDelta;
+            }
+        }
+
+        Move();
+        
+    }
+
+    void OnMouseOver()
+    {
+        if (!CanRun)
+        {
+            return;
+        }
+
+        if (IsAvoiding)
+        {
+            return;
+        }
+        IsAvoiding = true;
+        MoveByPush();
+        AvoidEnemyMouse();
+        CatchedByMouse = true;
+        IsAvoiding = false;
+    }
+
+
+    void OnMouseExit()
+    {
+    }
+
+    public void Move()
+    {
+        //transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -10f, 10f), transform.position.z);
+        var translation = Direction * Time.deltaTime * Speed * _currentAcceleration;
+        transform.Translate(translation);
+    }
+
+    public bool IsMouseDistanceSecure()
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var distance = Vector2.Distance(transform.position, worldPosition);
+        return distance > SecureMouseDistance;
+
+    }
+
+    void MoveByPush()
+    {
+        if (Speed > 0)
+        {
+            Speed *= -1;
+        }
+
+        _currentAcceleration = 2;
+        Move();
+    }
+    public void AvoidEnemyMouse()
+    {
+        var coefY = _random.Next(0, 101) % 2 == 0 ? 1 : -1;
+        var dirY = 2;
+        Direction = new Vector2(1, (float) (coefY* dirY));
+        Speed = Math.Abs(Speed);
+        _currentAcceleration = MinAcceleration;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!CanRun)
+        {
+            return;
+        }
+        if (other.gameObject.Equals(Bottom))
+        {
+            IsAvoiding = true;
+            Direction = new Vector2(transform.position.x + 1, 0);
+            Debug.LogError($"Caught bottom border");
+        }
+        if (other.gameObject.Equals(Top))
+        {
+            IsAvoiding = true;
+            Direction = new Vector2(transform.position.x + 1, 0);
+            Debug.LogError($"Caught top  border");
+        }
+
+        if (IsAvoiding)
+        {
+            _currentAcceleration = MinAcceleration;
+            Move();
+            ResetTranslationParams();
+            IsAvoiding = false;
+        }
+    }
+
+
+    public void ResetTranslationParams()
+    {
+        Direction = Vector2.right;
+        _currentAcceleration = DefaultAcceleration;
+    }
+
+    public void Reset()
+    {
+        CatchedByMouse = false;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameController : MonoBehaviour
+    {
+        public bool IsGameInProcess;
+        public BallController BallController;
+        public GameObject Ball;
+        public GameObject StartPointObject;
+        public GameObject FinishPointObject;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartNewGame();
+        }
+
+        public void StartNewGame()
+        {
+            if (StartPointObject == null)
+            {
+                Debug.LogWarning("Отсутствует ссылка на объект старта");
+                return;
+            }
+            if (FinishPointObject == null)
+            {
+                Debug.LogWarning("Отсутствует ссылка на объект финиша");
+                return;
+            }
+
+            StartPointObject.transform.position = new Vector3(-10, 0);
+            FinishPointObject.transform.position = new Vector3(10,0);
+            BallController.ResetPosition(StartPointObject.transform.position);
+            BallController.Reset();
+            BallController.CanRun = true;
+            IsGameInProcess = true;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                Restart();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.Equals(Ball))
+            {
+                BallController.CanRun = false;
+                IsGameInProcess = false;
+                Debug.LogError($"Game was finished");
+            }
+            else
+            {
+                Debug.Log($"{other.name} object triggered");
+            }
+        }
+
+        public void Restart()
+        {
+            IsGameInProcess = false;
+            BallController.CanRun = false;
+            StartNewGame();
+        }
+
+    }
+}
